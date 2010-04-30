cd54d09084d90a4a3f853f59165c81f4abacbccb
Virgil Dupras <hsoft@hardcoded.net>
2010-04-30 10:26:14 +0200

    Nose-ified currency_test.

--- tests/currency_test.py
+++ tests/currency_test.py
@@ -8,176 +8,181 @@
 
 from __future__ import division
 
-import copy
-import pickle
 from datetime import date
 import sqlite3 as sqlite
 
-from .. import testcase, io
-from ..path import Path
+from nose.tools import eq_, assert_almost_equal, with_setup
+
+from .. import io
 from ..currency import Currency, RatesDB, CAD, EUR, PLN, USD
+from ..testutil import with_tmpdir
 
+def teardown():
+    Currency.set_rates_db(None)
 
-class TestCase(testcase.TestCase):
-    def tearDown(self):
-        Currency.set_rates_db(None)
+@with_setup(None, teardown)
+def test_currency_creation():
+    # Different ways to create a currency.
+    eq_(Currency('CAD'), CAD)
+    eq_(Currency(name='Canadian dollar'), CAD)
 
+@with_setup(None, teardown)
+def test_currency_copy():
+    # Currencies can be copied.
+    import copy
+    eq_(copy.copy(CAD), CAD)
+    eq_(copy.deepcopy(CAD), CAD)
 
-class CurrencyTest(TestCase):
-    def test_creation(self):
-        """Different ways to create a currency."""
-        self.assertEqual(Currency('CAD'), CAD)
-        self.assertEqual(Currency(name='Canadian dollar'), CAD)
+@with_setup(None, teardown)
+def test_get_rate_on_empty_db():
+    # When there is no data available, use the fallback rate.
+    eq_(CAD.value_in(USD, date(2008, 4, 20)), 1 / USD.fallback_rate)
 
-    def test_copy(self):
-        """Currencies can be copied."""
-        self.assertEqual(copy.copy(CAD), CAD)
-        self.assertEqual(copy.deepcopy(CAD), CAD)
+@with_setup(None, teardown)
+@with_tmpdir
+def test_physical_rates_db_remember_rates(tmppath):
+    # When a rates db uses a real file, rates are remembered
+    dbpath = tmppath + 'foo.db'
+    db = RatesDB(dbpath)
+    db.set_CAD_value(date(2008, 4, 20), 'USD', 1/0.996115)
+    db = RatesDB(dbpath)
+    assert_almost_equal(db.get_rate(date(2008, 4, 20), 'CAD', 'USD'), 0.996115)
 
+@with_setup(None, teardown)
+def test_db_with_connection():
+    # the supplied connection is used by the rates db.
+    con = sqlite.connect(':memory:')
+    db = RatesDB(con)
+    try:
+        con.execute("select * from rates where 1=2")
+    except sqlite.OperationalError: # new db
+        raise AssertionError()
 
-class EmptyMemoryDB(TestCase):
-    def test_get_rate(self):
-        """When there is no data available, use the fallback rate"""
-        self.assertEqual(CAD.value_in(USD, date(2008, 4, 20)), 1 / USD.fallback_rate)
-    
+@with_setup(None, teardown)
+@with_tmpdir
+def test_corrupt_db(tmppath):
+    dbpath = tmppath + 'foo.db'
+    fh = io.open(dbpath, 'w')
+    fh.write('corrupted')
+    fh.close()
+    db = RatesDB(dbpath) # no crash. deletes the old file and start a new db
+    db.set_CAD_value(date(2008, 4, 20), 'USD', 42)
+    db = RatesDB(dbpath)
+    eq_(db.get_rate(date(2008, 4, 20), 'USD', 'CAD'), 42)
 
-class DBWithDailyRate(TestCase):
-    def setUp(self):
-        USD.set_CAD_value(1/0.996115, date(2008, 4, 20))
-    
-    def test_get_rate(self):
-        """Getting the rate exactly as set_rate happened returns the same rate"""
-        self.assertAlmostEqual(CAD.value_in(USD, date(2008, 4, 20)), 0.996115)
-    
-    def test_get_rate_different_currency(self):
-        """Use fallback rates when necessary"""
-        self.assertEqual(CAD.value_in(EUR, date(2008, 4, 20)), 1 / EUR.fallback_rate)
-        self.assertEqual(EUR.value_in(USD, date(2008, 4, 20)), EUR.fallback_rate * 0.996115)
-    
-    def test_get_rate_reverse(self):
-        """It's possible to get the reverse value of a rate using the same data"""
-        self.assertAlmostEqual(USD.value_in(CAD, date(2008, 4, 20)), 1 / 0.996115)
-    
-    def test_set_rate_twice(self):
-        """When setting a rate for an index that already exists, the old rate is replaced by the new"""
-        USD.set_CAD_value(1/42, date(2008, 4, 20))
-        self.assertAlmostEqual(CAD.value_in(USD, date(2008, 4, 20)), 42)
-    
-    def test_set_rate_after_get(self):
-        # When setting a rate after a get of the same rate, the rate cache is correctly updated.
-        CAD.value_in(USD, date(2008, 4, 20)) # value will be cached
-        USD.set_CAD_value(1/42, date(2008, 4, 20))
-        self.assertAlmostEqual(CAD.value_in(USD, date(2008, 4, 20)), 42)
-    
+#--- Daily rate
+def setup_daily_rate():
+    USD.set_CAD_value(1/0.996115, date(2008, 4, 20))
 
-class DBWithTwoDailyRates(TestCase):
-    def setUp(self):
-        # Don't change the set order, it's important for the tests
-        USD.set_CAD_value(1/0.997115, date(2008, 4, 25))
-        USD.set_CAD_value(1/0.996115, date(2008, 4, 20))
-    
-    def test_date_range_range(self):
-        """USD.rates_date_range() returns the USD's limits"""
-        self.assertEqual(USD.rates_date_range(), (date(2008, 4, 20), date(2008, 4, 25)))
-    
-    def test_date_range_for_unfetched_currency(self):
-        """If the curency is not in the DB, return None"""
-        self.assertTrue(PLN.rates_date_range() is None)
-    
-    def test_seek_rate_middle(self):
-        """A rate request with seek in the middle will return the lowest date"""
-        self.assertEqual(USD.value_in(CAD, date(2008, 4, 24)), 1/0.996115)
-    
-    def test_seek_rate_after(self):
-        """Make sure that the *nearest* lowest rate is returned. Because the 25th have been set 
-        before the 20th, an order by clause is required in the seek SQL to make this test pass"""
-        self.assertEqual(USD.value_in(CAD, date(2008, 4, 26)), 1/0.997115)
-    
-    def test_seek_rate_before(self):
-        """If there are no rate in the past, seek for a rate in the future"""
-        self.assertEqual(USD.value_in(CAD, date(2008, 4, 19)), 1/0.996115)
-    
+@with_setup(setup_daily_rate, teardown)
+def test_get_rate_with_daily_rate():
+    # Getting the rate exactly as set_rate happened returns the same rate.
+    assert_almost_equal(CAD.value_in(USD, date(2008, 4, 20)), 0.996115)
 
-class DBWithMutlipleCurrencies(TestCase):
-    def setUp(self):
-        USD.set_CAD_value(1/0.996115, date(2008, 4, 20))
-        EUR.set_CAD_value(1/0.633141, date(2008, 4, 20))
-    
-    def test_get_rate(self):
-        """Don't mix currency rates up"""
-        self.assertAlmostEqual(CAD.value_in(USD, date(2008, 4, 20)), 0.996115)
-        self.assertAlmostEqual(CAD.value_in(EUR, date(2008, 4, 20)), 0.633141)
-    
-    def test_get_rate_with_pivotal(self):
-        """It's possible to get a rate by using 2 records"""
-        # if 1 CAD = 0.996115 USD and 1 CAD = 0.633141 then 0.996115 USD = 0.633141 then 1 USD = 0.633141 / 0.996115 EUR
-        self.assertAlmostEqual(USD.value_in(EUR, date(2008, 4, 20)), 0.633141 / 0.996115)
-    
-    def test_get_rate_doesnt_exist(self):
-        """Don't crash when trying to do pivotal calculation with non-existing currencies"""
-        self.assertEqual(USD.value_in(PLN, date(2008, 4, 20)), 1 / 0.996115 / PLN.fallback_rate)
+@with_setup(setup_daily_rate, teardown)
+def test_get_rate_different_currency():
+    # Use fallback rates when necessary.
+    eq_(CAD.value_in(EUR, date(2008, 4, 20)), 1 / EUR.fallback_rate)
+    eq_(EUR.value_in(USD, date(2008, 4, 20)), EUR.fallback_rate * 0.996115)
 
+@with_setup(setup_daily_rate, teardown)
+def test_get_rate_reverse():
+    # It's possible to get the reverse value of a rate using the same data.
+    assert_almost_equal(USD.value_in(CAD, date(2008, 4, 20)), 1 / 0.996115)
 
-class DBWithPath(TestCase):
-    def setUp(self):
-        self.dbpath = Path(self.tmpdir()) + 'foo.db'
-        db = RatesDB(self.dbpath)
-        db.set_CAD_value(date(2008, 4, 20), 'USD', 1/0.996115)
-    
-    def test_get_rate_new_instance(self):
-        """Use another instance to call get_rate on"""
-        db = RatesDB(self.dbpath)
-        self.assertAlmostEqual(db.get_rate(date(2008, 4, 20), 'CAD', 'USD'), 0.996115)
-    
+@with_setup(setup_daily_rate, teardown)
+def test_set_rate_twice():
+    # When setting a rate for an index that already exists, the old rate is replaced by the new.
+    USD.set_CAD_value(1/42, date(2008, 4, 20))
+    assert_almost_equal(CAD.value_in(USD, date(2008, 4, 20)), 42)
 
-class DBWithConnection(TestCase):
-    def setUp(self):
-        self.con = sqlite.connect(':memory:')
-        self.db = RatesDB(self.con)
-    
-    def test_tables(self):
-        """the supplied connection is used instead"""
-        try:
-            self.con.execute("select * from rates where 1=2")
-        except sqlite.OperationalError: # new db
-            self.fail()
-    
+@with_setup(setup_daily_rate, teardown)
+def test_set_rate_after_get():
+    # When setting a rate after a get of the same rate, the rate cache is correctly updated.
+    CAD.value_in(USD, date(2008, 4, 20)) # value will be cached
+    USD.set_CAD_value(1/42, date(2008, 4, 20))
+    assert_almost_equal(CAD.value_in(USD, date(2008, 4, 20)), 42)
 
-class CorruptPath(TestCase):
-    def setUp(self):
-        self.dbpath = self.tmppath() + 'foo.db'
-        fh = io.open(self.dbpath, 'w')
-        fh.write('corrupted')
-        fh.close()
-    
-    def test_open_db(self):
-        db = RatesDB(self.dbpath) # no crash. deletes the old file and start a new db
-        db.set_CAD_value(date(2008, 4, 20), 'USD', 42)
-        db = RatesDB(self.dbpath)
-        self.assertEqual(db.get_rate(date(2008, 4, 20), 'USD', 'CAD'), 42)
-    
+#--- Two daily rates
+def setup_two_daily_rate():
+    # Don't change the set order, it's important for the tests
+    USD.set_CAD_value(1/0.997115, date(2008, 4, 25))
+    USD.set_CAD_value(1/0.996115, date(2008, 4, 20))
 
-class DBProblemAfterConnection(TestCase):
-    def setUp(self):
-        class MockConnection(sqlite.Connection): # can't mock sqlite3.Connection's attribute, so we subclass it
-            mocking = False
-            def execute(self, *args, **kwargs):
-                if self.mocking:
-                    raise sqlite.OperationalError()
-                else:
-                    return sqlite.Connection.execute(self, *args, **kwargs)
-            
-        
-        con = MockConnection(':memory:')
-        self.db = RatesDB(con)
-        con.mocking = True
+@with_setup(setup_two_daily_rate, teardown)
+def test_date_range_range():
+    # USD.rates_date_range() returns the USD's limits.
+    eq_(USD.rates_date_range(), (date(2008, 4, 20), date(2008, 4, 25)))
+
+@with_setup(setup_two_daily_rate, teardown)
+def test_date_range_for_unfetched_currency():
+    # If the curency is not in the DB, return None.
+    assert PLN.rates_date_range() is None
+
+@with_setup(setup_two_daily_rate, teardown)
+def test_seek_rate_middle():
+    # A rate request with seek in the middle will return the lowest date.
+    eq_(USD.value_in(CAD, date(2008, 4, 24)), 1/0.996115)
+
+@with_setup(setup_two_daily_rate, teardown)
+def test_seek_rate_after():
+    # Make sure that the *nearest* lowest rate is returned. Because the 25th have been set 
+    # before the 20th, an order by clause is required in the seek SQL to make this test pass.
+    eq_(USD.value_in(CAD, date(2008, 4, 26)), 1/0.997115)
+
+@with_setup(setup_two_daily_rate, teardown)
+def test_seek_rate_before():
+    # If there are no rate in the past, seek for a rate in the future.
+    eq_(USD.value_in(CAD, date(2008, 4, 19)), 1/0.996115)
+
+#--- Rates of multiple currencies
+def setup_rates_of_multiple_currencies():
+    USD.set_CAD_value(1/0.996115, date(2008, 4, 20))
+    EUR.set_CAD_value(1/0.633141, date(2008, 4, 20))
+
+@with_setup(setup_rates_of_multiple_currencies, teardown)
+def test_get_rate_multiple_currencies():
+    # Don't mix currency rates up.
+    assert_almost_equal(CAD.value_in(USD, date(2008, 4, 20)), 0.996115)
+    assert_almost_equal(CAD.value_in(EUR, date(2008, 4, 20)), 0.633141)
+
+@with_setup(setup_rates_of_multiple_currencies, teardown)
+def test_get_rate_with_pivotal():
+    # It's possible to get a rate by using 2 records.
+    # if 1 CAD = 0.996115 USD and 1 CAD = 0.633141 then 0.996115 USD = 0.633141 then 1 USD = 0.633141 / 0.996115 EUR
+    assert_almost_equal(USD.value_in(EUR, date(2008, 4, 20)), 0.633141 / 0.996115)
+
+@with_setup(setup_rates_of_multiple_currencies, teardown)
+def test_get_rate_doesnt_exist():
+    # Don't crash when trying to do pivotal calculation with non-existing currencies.
+    eq_(USD.value_in(PLN, date(2008, 4, 20)), 1 / 0.996115 / PLN.fallback_rate)
+
+#--- Problems after connection
+def get_problematic_db():
+    class MockConnection(sqlite.Connection): # can't mock sqlite3.Connection's attribute, so we subclass it
+        mocking = False
+        def execute(self, *args, **kwargs):
+            if self.mocking:
+                raise sqlite.OperationalError()
+            else:
+                return sqlite.Connection.execute(self, *args, **kwargs)
     
-    def test_date_range(self):
-        self.db.date_range('USD') # no crash
-    
-    def test_get_rate(self):
-        self.db.get_rate(date(2008, 4, 20), 'USD', 'CAD') # no crash
-    
-    def test_set_rate(self):
-        self.db.set_CAD_value(date(2008, 4, 20), 'USD', 42) # no crash
-    
+    con = MockConnection(':memory:')
+    db = RatesDB(con)
+    con.mocking = True
+    return db
+
+@with_setup(None, teardown)
+def test_date_range_with_problematic_db():
+    db = get_problematic_db()
+    db.date_range('USD') # no crash
+
+@with_setup(None, teardown)
+def test_get_rate_with_problematic_db():
+    db = get_problematic_db()
+    db.get_rate(date(2008, 4, 20), 'USD', 'CAD') # no crash
+
+@with_setup(None, teardown)
+def test_set_rate_with_problematic_db():
+    db = get_problematic_db()
+    db.set_CAD_value(date(2008, 4, 20), 'USD', 42) # no crash
