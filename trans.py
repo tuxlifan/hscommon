34814427b0ae96d754b45c11f9f862771b2e9d28
Virgil Dupras <hsoft@hardcoded.net>
2011-01-19 09:46:04 +0100

    Added Qt translation helper functions.

--- trans.py
+++ trans.py
@@ -9,6 +9,10 @@
 # Doing i18n with GNU gettext for the core text gets complicated, so what I do is that I make the
 # GUI layer responsible for supplying a tr() function.
 
+import sys
+import locale
+import logging
+
 _trfunc = None
 
 def tr(s, context=None):
@@ -33,5 +37,32 @@ def install_cocoa_trans():
     currentLang = NSBundle.preferredLocalizationsFromArray_(mainBundle.localizations())[0]
     LANG2LOCALENAME = {'fr': 'fr_FR', 'de': 'de_DE'}
     if currentLang in LANG2LOCALENAME:
-        import locale
         locale.setlocale(locale.LC_ALL, LANG2LOCALENAME[currentLang])
+
+def install_qt_trans(lang=None):
+    from PyQt4.QtCore import QCoreApplication, QTranslator, QLocale
+    if sys.platform == 'win32':
+        LANG2LOCALENAME = {'fr': 'fra_fra', 'de': 'deu_deu'}
+    elif sys.platform == 'darwin':
+        LANG2LOCALENAME = {'fr': 'fr_FR', 'de': 'de_DE'}
+    else:
+        LANG2LOCALENAME = {'fr': 'fr_FR.UTF-8', 'de': 'de_DE.UTF-8'}
+    if not lang:
+        lang = str(QLocale.system().name())[:2]
+    if lang in LANG2LOCALENAME:
+        localeName = LANG2LOCALENAME[lang]
+        try:
+            locale.setlocale(locale.LC_ALL, str(localeName))
+        except locale.Error:
+            logging.warning("Couldn't set locale %s", localeName)
+    else:
+        lang = 'en'
+    qtr1 = QTranslator(QCoreApplication.instance())
+    qtr1.load(':/qt_%s' % lang)
+    QCoreApplication.installTranslator(qtr1)
+    qtr2 = QTranslator(QCoreApplication.instance())
+    qtr2.load(':/%s' % lang)
+    QCoreApplication.installTranslator(qtr2)
+    def qt_tr(s, context='core'):
+        return str(QCoreApplication.translate(context, s, None))
+    set_tr(qt_tr)
