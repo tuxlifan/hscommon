f45885fa26e4db162ef218106ddec3f9960f7ff9
Virgil Dupras <hsoft@hardcoded.net>
2011-04-16 03:27:10 -0700

    Updated a couple of remaning copyright years (in about boxes) from 2010 to 2011.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'hscommon'
-copyright = '2010, Hardcoded Software'
+copyright = '2011, Hardcoded Software'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
