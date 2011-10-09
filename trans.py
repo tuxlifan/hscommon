f10ca0544697edc2aeb61bc1341e4c1f97ef9c15
Virgil Dupras <hsoft@hardcoded.net>
2011-10-09 14:59:34 -0400

    Added Czech to the list of supported locales in trans.

--- trans.py
+++ trans.py
@@ -42,9 +42,9 @@ def set_tr(new_tr):
 def get_locale_name(lang):
     if ISWINDOWS:
         # http://msdn.microsoft.com/en-us/library/39cwe7zf(vs.71).aspx
-        LANG2LOCALENAME = {'fr': 'fra', 'de': 'deu', 'it': 'ita', 'zh_CN': 'chs'}
+        LANG2LOCALENAME = {'fr': 'fra', 'de': 'deu', 'it': 'ita', 'zh_CN': 'chs', 'cs': 'czy'}
     else:
-        LANG2LOCALENAME = {'fr': 'fr_FR', 'de': 'de_DE', 'it': 'it_IT', 'zh_CN': 'zh_CN'}
+        LANG2LOCALENAME = {'fr': 'fr_FR', 'de': 'de_DE', 'it': 'it_IT', 'zh_CN': 'zh_CN', 'cs': 'cs_CZ'}
     if lang not in LANG2LOCALENAME:
         return None
     result = LANG2LOCALENAME[lang]
