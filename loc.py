b63865ec19d4e940f975c34f52cecc2983a3fc87
Virgil Dupras <hsoft@hardcoded.net>
2011-11-02 16:45:21 -0400

    Added mechanism to create lproj folders from a base en.lproj and a .po file.

--- loc.py
+++ loc.py
@@ -9,10 +9,10 @@ from . import pygettext
 
 LC_MESSAGES = 'LC_MESSAGES'
 
-def _get_langs(folder):
+def get_langs(folder):
     return [name for name in os.listdir(folder) if op.isdir(op.join(folder, name))]
 
-def _files_with_ext(folder, ext):
+def files_with_ext(folder, ext):
     return [op.join(folder, fn) for fn in os.listdir(folder) if fn.endswith(ext)]
 
 def generate_pot(folders, outpath, keywords):
@@ -24,39 +24,47 @@ def generate_pot(folders, outpath, keywords):
     pygettext.main(pyfiles, outpath=outpath, keywords=keywords)
 
 def compile_all_po(base_folder):
-    langs = _get_langs(base_folder)
+    langs = get_langs(base_folder)
     for lang in langs:
         pofolder = op.join(base_folder, lang, LC_MESSAGES)
-        pofiles = _files_with_ext(pofolder, '.po')
+        pofiles = files_with_ext(pofolder, '.po')
         for pofile in pofiles:
             p = polib.pofile(pofile)
             p.save_as_mofile(pofile[:-3] + '.mo')
 
 def merge_locale_dir(target, mergeinto):
-    langs = _get_langs(target)
+    langs = get_langs(target)
     for lang in langs:
         if not op.exists(op.join(mergeinto, lang)):
             continue
         mofolder = op.join(target, lang, LC_MESSAGES)
-        mofiles = _files_with_ext(mofolder, '.mo')
+        mofiles = files_with_ext(mofolder, '.mo')
         for mofile in mofiles:
             shutil.copy(mofile, op.join(mergeinto, lang, LC_MESSAGES))
 
 def merge_pots_into_pos(folder):
     # We're going to take all pot files in `folder` and for each lang, merge it with the po file
     # with the same name.
-    potfiles = _files_with_ext(folder, '.pot')
+    potfiles = files_with_ext(folder, '.pot')
     for potfile in potfiles:
         refpot = polib.pofile(potfile)
         refname = op.splitext(op.basename(potfile))[0]
-        for lang in _get_langs(folder):
+        for lang in get_langs(folder):
             po = polib.pofile(op.join(folder, lang, LC_MESSAGES, refname + '.po'))
             po.merge(refpot)
             po.save()
 
 #--- Cocoa
 def all_lproj_paths(folder):
-    return _files_with_ext(folder, '.lproj')
+    return files_with_ext(folder, '.lproj')
+
+def all_xib_strings(lprojpath):
+    xibnames = [op.splitext(op.basename(fn))[0] for fn in os.listdir(lprojpath) if fn.endswith('.xib')]
+    stringspaths = [op.join(lprojpath, fn + '.strings') for fn in xibnames]
+    return [path for path in stringspaths if op.exists(path)]
+
+def escape_cocoa_strings(s):
+    return s.replace('\\', '\\\\').replace('"', '\\"').replace('\n', '\\n')
 
 def unescape_cocoa_strings(s):
     return s.replace('\\\\', '\\').replace('\\"', '"').replace('\\n', '\n')
@@ -79,7 +87,7 @@ def strings2pot(target, dest):
     po.save()
 
 def allstrings2pot(lprojpath, dest, excludes=None):
-    allstrings = _files_with_ext(lprojpath, '.strings')
+    allstrings = files_with_ext(lprojpath, '.strings')
     if excludes:
         allstrings = [p for p in allstrings if op.splitext(op.basename(p))[0] not in excludes]
     for strings_path in allstrings:
@@ -122,7 +130,7 @@ def strings2po(en_strings, tr_strings, dest):
     po.save()
 
 def allstrings2po(enlproj, trlproj, dest, excludes=None):
-    allstrings = _files_with_ext(trlproj, '.strings')
+    allstrings = files_with_ext(trlproj, '.strings')
     for stringsfile in allstrings:
         basename = op.splitext(op.basename(stringsfile))[0]
         if excludes and basename in excludes:
@@ -130,3 +138,30 @@ def allstrings2po(enlproj, trlproj, dest, excludes=None):
         enstrings = op.join(enlproj, basename + '.strings')
         strings2po(enstrings, stringsfile, dest)
 
+def po2strings(pofile, en_strings, dest):
+    # Takes en_strings and replace all righthand parts of "foo" = "bar"; entries with translations
+    # in pofile, then puts the result in dest.
+    po = polib.pofile(pofile)
+    with open(en_strings, 'rt', encoding='utf-8') as fp:
+        contents = fp.read()
+    re_trans = re.compile(r'(?<= = ").*(?=";\n)')
+    def repl(match):
+        s = match.group(0)
+        unescaped = unescape_cocoa_strings(s)
+        entry = po.find(unescaped)
+        if entry is None:
+            print("WARNING: Could not find entry '{}' in .po file".format(s))
+            return s
+        trans = entry.msgstr
+        return escape_cocoa_strings(trans) if trans else s
+    contents = re_trans.sub(repl, contents)
+    with open(dest, 'wt', encoding='utf-8') as fp:
+        fp.write(contents)
+
+def po2allxibstrings(pofile, en_lproj, dest_lproj):
+    if not op.exists(dest_lproj):
+        os.mkdir(dest_lproj)
+    allstrings = all_xib_strings(en_lproj)
+    for strings in allstrings:
+        deststrings = op.join(dest_lproj, op.basename(strings))
+        po2strings(pofile, strings, deststrings)
