1f8479b89f8b8abc5079ddc63ca4089f18514815
Virgil Dupras <hsoft@hardcoded.net>
2011-11-02 15:52:17 -0400

    Added a mechanism to transfer translations from XIB strings to .po files.

--- loc.py
+++ loc.py
@@ -68,12 +68,10 @@ def strings2pot(target, dest):
     re_trans = re.compile(r'".*" = "(.*)";')
     strings = re_trans.findall(contents)
     po = polib.pofile(dest)
-    addcount = 0
     for s in strings:
         s = unescape_cocoa_strings(s)
         entry = po.find(s)
         if entry is None:
-            addcount += 1
             entry = polib.POEntry(msgid=s)
             po.append(entry)
         # we don't know or care about a line number so we put 0
@@ -86,3 +84,49 @@ def allstrings2pot(lprojpath, dest, excludes=None):
         allstrings = [p for p in allstrings if op.splitext(op.basename(p))[0] not in excludes]
     for strings_path in allstrings:
         strings2pot(strings_path, dest)
+
+def strings2po(en_strings, tr_strings, dest):
+    # This function is a temporary function to get translation information from XIB strings files
+    # into .po files. Because XIB strings look like "123.title" = "foobar";, we need the reference
+    # english strings file to know what the english title of object 123 actually is. We could rely
+    # on the comment just before every entry, but it seems like newlines and other special
+    # characters are ommited from the comment, so it messes up our stuff.
+    # Once all cocoa strings have been translated and I only receive translations in the new .po
+    # format, this function won't be needed anymore.
+    print("Processing {} with {} onto {}".format(tr_strings, en_strings, dest))
+    re_trans = re.compile(r'"(.*)" = "(.*)";')
+    with open(en_strings, 'rt', encoding='utf-8') as fp:
+        contents = fp.read()
+    # ref2en = {123.title: en}
+    ref2en = {ref: unescape_cocoa_strings(en) for ref, en in re_trans.findall(contents)}
+    with open(tr_strings, 'rt', encoding='utf-8') as fp:
+        contents = fp.read()
+    # ref2tr = {123.title: translated}
+    ref2tr = {ref: unescape_cocoa_strings(tr) for ref, tr in re_trans.findall(contents)}
+    po = polib.pofile(dest)
+    print("Untranslated entries: {}".format(len(po.untranslated_entries())))
+    count = 0
+    for ref, en in ref2en.items():
+        translated = ref2tr.get(ref)
+        if not translated:
+            continue
+        entry = po.find(en)
+        assert entry is not None # supposed to be in the .pot and .po
+        if entry.msgstr:
+            if entry.msgstr != translated:
+                print("WARNING: {} != {} for {}".format(translated, entry.msgstr, en))
+        else:
+            entry.msgstr = translated
+            count += 1
+    print("Updated {} translations".format(count))
+    po.save()
+
+def allstrings2po(enlproj, trlproj, dest, excludes=None):
+    allstrings = _files_with_ext(trlproj, '.strings')
+    for stringsfile in allstrings:
+        basename = op.splitext(op.basename(stringsfile))[0]
+        if excludes and basename in excludes:
+            continue
+        enstrings = op.join(enlproj, basename + '.strings')
+        strings2po(enstrings, stringsfile, dest)
+
